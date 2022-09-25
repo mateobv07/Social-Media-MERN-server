829ef715975fd23416db7e128bed49aec370b02a
unknown <bernasconivargas.mateo@gmail.com>
2022-09-25 11:10:55 -0500

    get sinular post route

--- controllers/posts.js
+++ controllers/posts.js
@@ -17,6 +17,22 @@ export const getPosts = async (req, res) => {
     }
 }
 
+export const getPost = async (req, res) => {
+    const { page } = req.query;
+    try {
+        const LIMIT = 8;
+        const startIndex = (Number(page) - 1) * LIMIT; //Get starting index of every page
+        const total = await PostMessage.countDocuments({});
+
+        //filter from new->old, only get LIMIT amount statring from index
+        const posts = await PostMessage.find().sort({ _id: -1}).limit(LIMIT).skip(startIndex);
+
+        res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
 export const getPostsBySearch = async (req, res) => {
     const { searchQuery, tags } = req.query;
 
