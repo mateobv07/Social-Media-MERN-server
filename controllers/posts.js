930d2591b92389c7565670d34d5daed355e359e1
unknown <bernasconivargas.mateo@gmail.com>
2022-09-26 17:21:03 -0500

    post detail controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -18,16 +18,12 @@ export const getPosts = async (req, res) => {
 }
 
 export const getPost = async (req, res) => {
-    const { page } = req.query;
+    const { id } = req.params;
     try {
-        const LIMIT = 8;
-        const startIndex = (Number(page) - 1) * LIMIT; //Get starting index of every page
-        const total = await PostMessage.countDocuments({});
+       
+        const post = await PostMessage.findById(id);
 
-        //filter from new->old, only get LIMIT amount statring from index
-        const posts = await PostMessage.find().sort({ _id: -1}).limit(LIMIT).skip(startIndex);
-
-        res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
+        res.status(200).json(post);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
